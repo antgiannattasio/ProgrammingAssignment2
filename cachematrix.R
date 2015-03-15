6ab2c212bc7ab617e68c64f4e9b6b85b784843d2
antgiannattasio <ant.giannattasio@gmail.com>
2015-03-15 17:24:28 +0100

    Some explanatory changes
    .... but not different code...

--- cachematrix.R
+++ cachematrix.R
@@ -3,9 +3,10 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { 
 
-        m <- NULL
+        m <- NULL                      #Here the functions is identical to the makeVector(), only
+                                        #the argument of the function changes from x= numeric() to x=matrix()
         set <- function(y) {
                 x <<- y
                 m <<- NULL
@@ -19,12 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
         
 }
 
-cachemean <- function(x = matrix(), ...) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
+cachemean <- function(x = matrix(), ...) {  #the function is very similar to the chachemean, I retained the same name
+        m <- x$getmean()                    #to not incur some bug and as programmerssay... "a good programmer is a lazy
+        if(!is.null(m)) {                   #programmer.... So why should somebody change something that already works as
+                message("getting cached data") #it is.... only a food can do that.... so now I will concentrate to how I can 
+                return(m)                   #finish this assignment writing the last function at the bottom of this file...
+        }                                   #Wish me good luck... Thank you for you attention :D
         data <- x$get()
         m <- solve(data, ...)
         x$setmean(m)
